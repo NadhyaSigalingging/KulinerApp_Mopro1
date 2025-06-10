1c754e63f59eb05c7227c586c7faa3c303cf47ea
NadhyaSigalingging <nadhyasigalingging@gmail.com>
2025-06-10 20:01:44 +0700

    Menangani gambar gagal di-load

--- app/src/main/java/com/nadhya0065/catatankuliner/ui/screen/MainScreen.kt
+++ app/src/main/java/com/nadhya0065/catatankuliner/ui/screen/MainScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -86,6 +87,8 @@ fun ListItem(kuliner: Kuliner){
                 .build(),
             contentDescription = stringResource(R.string.gambar,kuliner.nama_makanan),
             contentScale = ContentScale.Crop,
+            placeholder = painterResource(id = R.drawable.loading_img),
+            error = painterResource(id = R.drawable.baseline_broken_image_24),
             modifier = Modifier.fillMaxWidth().padding(4.dp)
         )
         Column (
