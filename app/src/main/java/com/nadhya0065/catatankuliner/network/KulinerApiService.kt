fb2f8ac63db0a72019e4200804f181b5b3f368e9
NadhyaSigalingging <nadhyasigalingging@gmail.com>
2025-06-10 18:10:41 +0700

    Memproses data string sebagai JSON

--- app/src/main/java/com/nadhya0065/catatankuliner/network/KulinerApiService.kt
+++ app/src/main/java/com/nadhya0065/catatankuliner/network/KulinerApiService.kt
@@ -1,20 +1,27 @@
 package com.nadhya0065.catatankuliner.network
 
+import com.nadhya0065.catatankuliner.model.Kuliner
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
 private const val BASE_URL = "https://store.sthresearch.site/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface KulinerApiService {
     @GET("food_review.php")
-    suspend fun getKuliner(): String
+    suspend fun getKuliner(): List<Kuliner>
 }
 
 object KulinerApi{
